d82d7f46d3c689dc9e32e765b6bd1e0b20f21838
emanuelosva <emanuelosva@gmail.com>
2021-04-19 22:48:43 -0500

    chore(Setup): config for relative import paths

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,24 @@ module.exports = {
   setupFiles: [
     './config.ts',
   ],
+  moduleDirectories: [
+    'node_modules',
+    'src',
+  ],
+  roots: [
+    'src',
+  ],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@common/(.*)': '<rootDir>/src/common/$1',
+    '@domains/(.*)': '<rootDir>/src/domains/$1',
+    '@infraestructure/(.*)': '<rootDir>/src/infraestructure/$1',
+    '@business/(.*)': '<rootDir>/src/domains/business/$1',
+    '@authentication/(.*)': '<rootDir>/src/domains/authentication/$1',
+    '@staffs/(.*)': '<rootDir>/src/domains/staffs/$1',
+    '@appointments/(.*)': '<rootDir>/src/domains/appointments/$1',
+    '@httpDelivery/(.*)': '<rootDir>/src/delivery/http/$1',
+  },
 }
